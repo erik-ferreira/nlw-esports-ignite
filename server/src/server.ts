6bdc119b25170b4ef66c8c037ba56e93cf5154a0
erik-ferreira <erikdesouzagf@gmail.com>
2022-09-30 00:33:46 -0300

    create routes list games, ads by game and discord by ad

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,70 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
-
-app.get("/games", (request, response) => {
-  return response.json([]);
+const prisma = new PrismaClient({
+  log: ["query"],
 });
 
-app.post("/ads", (request, response) => {
+app.get("/games", async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+
+  return response.json(games);
+});
+
+app.post("/ads", async (request, response) => {
   return response.status(201).json([]);
 });
 
-app.get("/games/:id/ads", (request, response) => {
-  // const gameId = request.params.id;
+app.get("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
 
-  return response.status(200).json([
-    { id: 1, name: "Anúncio 1" },
-    { id: 2, name: "Anúncio 2" },
-  ]);
+  const adsDb = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const ads = adsDb.map((ad) => ({ ...ad, weekDays: ad.weekDays.split(",") }));
+
+  return response.status(200).json(ads);
 });
 
-app.get("/ads/:id/discord", (request, response) => {
+app.get("/ads/:id/discord", async (request, response) => {
   const adId = request.params.id;
 
-  return response.status(200).json([]);
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+
+  return response.status(200).json({
+    discord: ad.discord,
+  });
 });
 
 app.listen(3333);

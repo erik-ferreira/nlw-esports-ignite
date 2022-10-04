20cb8d90c63c313c024357bce6e3ef160ca16d84
erik-ferreira <erikdesouzagf@gmail.com>
2022-10-03 22:38:52 -0300

    complete getListGames

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,23 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            "module-resolver",
+            {
+              root: ["./src"],
+              alias: {
+                "@components": "./src/components",
+              },
+            },
+          ],
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
